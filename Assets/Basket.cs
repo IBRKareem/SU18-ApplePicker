9d7e7f3ffc41e667bd76a1fb195294504dfee941
IBRKareem <36110628+IBRKareem@users.noreply.github.com>
2018-06-07 01:20:05 -0400

    Counters
    Added HighScore and Score Counters

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -4,12 +4,34 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
+using UnityEngine.UI;     // This line enables use of uGUI features.        // a
+
 
 
 public class Basket : MonoBehaviour {
 
+	[Header("Set Dynamically")]
+
+	public Text           scoreGT;                                          // a
 
 
+
+	void Start() {
+
+		// Find a reference to the ScoreCounter GameObject
+
+		GameObject scoreGO = GameObject.Find("ScoreCounter");               // b
+
+		// Get the Text Component of that GameObject
+
+		scoreGT = scoreGO.GetComponent<Text>();                             // c
+
+		// Set the starting number of points to 0
+
+		scoreGT.text = "0";
+
+	}
+		
 	void Update () {
 
 		// Get the current screen position of the mouse from Input
@@ -37,6 +59,34 @@ public class Basket : MonoBehaviour {
 		pos.x = mousePos3D.x;
 
 		this.transform.position = pos;
+
+	}
+
+	void OnCollisionEnter( Collision coll ) {                         // a
+
+		// Find out what hit this basket
+
+		GameObject collidedWith = coll.gameObject;                    // b
+
+		if ( collidedWith.tag == "Apple" ) {                          // c
+
+			Destroy( collidedWith );                                        
+
+
+
+			// Parse the text of the scoreGT into an int
+
+			int score = int.Parse( scoreGT.text );                          // d
+
+			// Add points for catching the apple
+
+			score += 100;
+
+			// Convert the score back to a string and display it
+
+			scoreGT.text = score.ToString();
+
+		}
 
 	}
 
